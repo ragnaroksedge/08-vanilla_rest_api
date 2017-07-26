e7473616f432ca48b2f400cf3c209c38f2c2ea10
Allan Liebold <allan.liebold@gmail.com>
2017-07-26 13:48:29 -0700

    creates package.json and sets up server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer();
+
+server.listen(PORT, () => {
+  console.log('server:', PORT);
+});
