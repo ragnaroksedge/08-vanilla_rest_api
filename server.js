e30405f4fa89361e26ca61a1f2ef5fd161ab7ac8
Allan Liebold <allan.liebold@gmail.com>
2017-07-26 14:18:13 -0700

    creates parse.json.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 'use strict';
 
 const http = require('http');
+// const Spell = require('./model/spell.js');
+// const Router = require('/.lib/router.js');
+// const storage = require('./lib/storage.js');
+// const router = new Router();
+
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer();
