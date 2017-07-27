f1b9b4a6fa6dd4d197bb6204edb15ddc48cb9a35
Allan Liebold <allan.liebold@gmail.com>
2017-07-26 23:53:14 -0700

    creates houses model, storage, and tests

--- server.js
+++ server.js
@@ -1,14 +1,61 @@
 'use strict';
 
 const http = require('http');
-// const Spell = require('./model/spell.js');
-// const Router = require('/.lib/router.js');
-// const storage = require('./lib/storage.js');
-// const router = new Router();
-
+const House = require('./model/houses.js');
+const storage = require('./lib/storage.js');
+const Router = require('./lib/router.js');
+const router = new Router();
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer();
+router.get('/api/houses', function(req, res) {
+  if(req.url.query.id) {
+    storage.fetchItem('house', req.url.query.id)
+    .then(house => {
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+
+      res.write(JSON.stringify(house));
+      res.end();
+    })
+    .catch(err => {
+      console.error(err);
+      res.writeHead(404, {
+        'Content-Type': 'text/plain'
+      });
+      res.write('house not found');
+      res.end();
+    });
+    return;
+  }
+
+  res.writeHead(400, {
+    'Content-Type': 'text/plain'
+  });
+  res.write('bad request');
+  res.end();
+});
+
+router.post('/api/houses', function(req, res) {
+  try {
+    var house = new House(req.body.name, req.body.seat, req.body.region, req.body.words);
+    storage.createItem('house', house);
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    res.write(JSON.stringify(house));
+    res.end();
+  } catch(err) {
+    console.error(err);
+    res.writeHead(400, {
+      'Content-Type': 'text/plain'
+    });
+    res.write('bad request');
+    res.end();
+  }
+});
+
+const server = http.createServer(router.route());
 
 server.listen(PORT, () => {
   console.log('server:', PORT);
---
+++ test/house-route-test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const request = require('superagent');
+const expect = require('chai').expect;
+
+require('../server.js');
+
+describe('House Routes', function() {
+  var house = null;
+
+  describe('POST: /api/houses', function() {
+    it('should return a house', function(done) {
+      request.post('localhost:8000/api/houses')
+      .send({ name: 'Stark', seat: 'Winterfell', region: 'The North', words: 'Winter is Coming' })
+      .end((err, res) => {
+        if(err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.name).to.equal('Stark');
+        expect(res.body.seat).to.equal('Winterfell');
+        expect(res.body.region).to.equal('The North');
+        expect(res.body.words).to.equal('Winter is Coming');
+        house = res.body;
+        done();
+      });
+    });
+  });
+
+  describe('GET: /api/houses', function() {
+    it('should return a house', function(done) {
+      request.get(`localhost:8000/api/houses?id=${house.id}`)
+      .end((err, res) => {
+        if(err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.name).to.equal('Stark');
+        expect(res.body.seat).to.equal('Winterfell');
+        expect(res.body.region).to.equal('The North');
+        expect(res.body.words).to.equal('Winter is Coming');
+        done();
+      });
+    });
+  });
+});
